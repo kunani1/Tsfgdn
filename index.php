9b65592c23d5469e2eb2afddb8bf713ad3577257
kunani1 <131540652+kunani1@users.noreply.github.com>
2024-10-07 11:53:10 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// URL of the data
+$url = "https://rahuketu.rf.gd";
+
+// Get the data from the URL
+$response = file_get_contents($url);
+
+// Check if the request was successful
+if ($response !== false) {
+    // Output the response
+    echo $response;
+} else {
+    echo "Failed to retrieve data from the URL.";
+}
+?>
